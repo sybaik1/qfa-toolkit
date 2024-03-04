8699558e47afce93b480dd48577925188f8ee00b
Sichoel Sung <sicheol.sung@yonsei.ac.kr>
2024-03-04 16:03:42 +0900

    Revert "test: Two-side check for moqfa equivalence."
    This reverts commit f239c7a5f9d6671fc557acaf107ab14c3d3edfb2.

--- tests/test_measure_once_quantum_finite_automaton.py
+++ tests/test_measure_once_quantum_finite_automaton.py
@@ -113,18 +113,16 @@ class TestMeasureOnceQuantumFiniteAutomaton(unittest.TestCase):
             m2 = self.get_moqfa(k2)
             m2.transitions = multiply_arbitrary_global_phase(m2.transitions)
             self.assertEqual(m1 == m2, k1 == k2)
-            self.assertEqual(m2 == m1, k2 == k1)
 
     def test_counter_example(self):
         for k1, k2 in itertools.product(self.qfa_parameters, repeat=2):
             m1 = self.get_moqfa(k1)
             m2 = self.get_moqfa(k2)
             m2.transitions = multiply_arbitrary_global_phase(m2.transitions)
-            counter_examples = [m1.counter_example(m2), m2.counter_example(m1)]
-            for counter_example in counter_examples:
-                if counter_example is not None:
-                    self.assertNotAlmostEqual(
-                        m1(counter_example), m2(counter_example))
+            counter_example = m1.counter_example(m2)
+            if counter_example is not None:
+                self.assertNotAlmostEqual(
+                    m1(counter_example), m2(counter_example))
 
 
 if __name__ == '__main__':
